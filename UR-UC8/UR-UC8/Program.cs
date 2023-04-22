0f0e5d24e6b2bde8fbfe5ef0876195a0116e1f13
MSRbuddy <meghanadasari222@gmail.com>
2023-04-22 12:31:31 +0530

    Added : USer Registration Program UC-8!

---
+++ UR-UC8/UR-UC8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UR_UC8
+{
+    internal class Program
+    {
+        public static string PwdRule4 = @"^[A-Z]{1,3}[a-z]{2,20}[-\#\$\@\%\&\+]?[0-9]{1,3}";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****Valid Password(Rule-4)!*****");
+            Console.WriteLine("------------------------------------");
+
+            Regex r = new Regex(Program.PwdRule4);
+            Console.WriteLine("\nEnter User Password(Rule4) : ");
+            Console.WriteLine(r.IsMatch(Console.ReadLine()));
+        }
+    }
+}
