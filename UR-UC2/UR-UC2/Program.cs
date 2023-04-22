40f1947be6c52ddf2e719acd6b157c055e1af0a9
MSRbuddy <meghanadasari222@gmail.com>
2023-04-22 12:24:00 +0530

    Added : USer Registration Program UC-2!

---
+++ UR-UC2/UR-UC2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UR_UC2
+{
+    internal class Program
+    {
+        public static string LastName = @"^[A-Z][a-z]{2,10}$";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Valid LastName*****!");
+            Console.WriteLine("----------------------------");
+
+            Regex r = new Regex(Program.LastName);
+            Console.WriteLine("\nEnter User Last Name: ");
+            Console.WriteLine(r.IsMatch(Console.ReadLine()));
+        }
+    }
+}
