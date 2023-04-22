e02d16119e704422cdbf8b5a64667c9d7af88b97
MSRbuddy <meghanadasari222@gmail.com>
2023-04-22 12:22:31 +0530

    Added : USer Registration Program UC-1!

---
+++ UR-UC1/UR-UC1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UR_UC1
+{
+    internal class Program
+    {
+        public static string FirstName = @"^[A-Z][a-z]{2,10}$";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Valid FirstName*****!");
+            Console.WriteLine("-----------------------------");
+
+            Regex r = new Regex(Program.FirstName);
+            Console.WriteLine("\nEnter User First Name: ");
+            Console.WriteLine(r.IsMatch(Console.ReadLine()));
+        }
+    }
+}
