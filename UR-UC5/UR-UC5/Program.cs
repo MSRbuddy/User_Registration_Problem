5f13a7ff588b88cfc337559baadea13ac59cec7e
MSRbuddy <meghanadasari222@gmail.com>
2023-04-22 12:28:16 +0530

    Added : USer Registration Program UC-5!

---
+++ UR-UC5/UR-UC5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UR_UC5
+{
+    internal class Program
+    {
+        public static string PwdRule1 = @"^[A-Za-z]{8,20}$";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Valid Password(Rule-1)*****!");
+            Console.WriteLine("------------------------------------");
+
+            Regex r = new Regex(Program.PwdRule1);
+            Console.WriteLine("\nEnter User Password(Rule1) : ");
+            Console.WriteLine(r.IsMatch(Console.ReadLine()));
+        }
+    }
+}
