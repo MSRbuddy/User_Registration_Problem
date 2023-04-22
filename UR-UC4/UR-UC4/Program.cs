c61e27909fd4fb9f5d3194d56ea3c6d8526c8f36
MSRbuddy <meghanadasari222@gmail.com>
2023-04-22 12:27:24 +0530

    Added : USer Registration Program UC-4!

---
+++ UR-UC4/UR-UC4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UR_UC4
+{
+    internal class Program
+    {
+        public static string PhoneNumber = @"^[9]?[1]?[ ][6-9]?[0-9]{9}$";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Valid Phone Number*****!");
+            Console.WriteLine("--------------------------------");
+
+            Regex r = new Regex(Program.PhoneNumber);
+            Console.WriteLine("\nEnter User Contact Number: ");
+            Console.WriteLine(r.IsMatch(Console.ReadLine()));
+        }
+    }
+}
