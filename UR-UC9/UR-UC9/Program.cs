c25da24860799e3a88fa32bf1320c7effcd2330a
MSRbuddy <meghanadasari222@gmail.com>
2023-04-22 12:32:27 +0530

    Added : USer Registration Program UC-9!

---
+++ UR-UC9/UR-UC9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UR_UC9
+{
+    internal class Program
+    {
+        public static string SampleEmail = @"^\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z$";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Valid Sample Email-Id!*****");
+            Console.WriteLine("-----------------------------------");
+
+            Regex r = new Regex(Program.SampleEmail);
+            Console.WriteLine("\nEnter User Sample Email: ");
+            Console.WriteLine(r.IsMatch(Console.ReadLine()));
+        }
+    }
+}
