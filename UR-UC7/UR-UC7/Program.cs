872845d572330ec4b97ffc335b9717035ba744a0
MSRbuddy <meghanadasari222@gmail.com>
2023-04-22 12:30:28 +0530

    Added : USer Registration Program UC-7!

---
+++ UR-UC7/UR-UC7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UR_UC7
+{
+    internal class Program
+    {
+        public static string PwdRule3 = @"^[A-Z]{1,3}[a-z]{2,20}[0-9]{1,3}$";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Valid Password(Rule-3)!*****");
+            Console.WriteLine("------------------------------------");
+
+            Regex r = new Regex(Program.PwdRule3);
+            Console.WriteLine("\nEnter User Password(Rule3) : ");
+            Console.WriteLine(r.IsMatch(Console.ReadLine()));
+        }
+    }
+}
