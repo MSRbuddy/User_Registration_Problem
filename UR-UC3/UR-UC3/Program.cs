d79c7e62ffb687f0a2f768c2f89079f38979bf6a
MSRbuddy <meghanadasari222@gmail.com>
2023-04-22 12:25:13 +0530

    Added : USer Registration Program UC-3!

---
+++ UR-UC3/UR-UC3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UR_UC3
+{
+    internal class Program
+    {
+        public static string MailId = @"^[a-z0-9|a-z0-9.a-z]@[a-z|a-z.a-z{2}]|[.a-z{2}]$";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Valid Email-Id*****!");
+            Console.WriteLine("-----------------------------");
+
+            Regex r = new Regex(Program.MailId);
+            Console.WriteLine("\nEnter User Email Id: ");
+            Console.WriteLine(r.IsMatch(Console.ReadLine()));
+        }
+    }
+}
