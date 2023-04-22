cbac5ca7a86adc153ae3d8db3e0b74091745fcf9
MSRbuddy <meghanadasari222@gmail.com>
2023-04-22 12:29:30 +0530

    Added : USer Registration Program UC-6!

---
+++ UR-UC6/UR-UC6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UR_UC6
+{
+    internal class Program
+    {
+        public static string PwdRule2 = @"^[A-Z]{1,3}[a-z]{5,20}$";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Valid Password(Rule-2)!*****");
+            Console.WriteLine("------------------------------------");
+
+            Regex r = new Regex(Program.PwdRule2);
+            Console.WriteLine("\nEnter User Password(Rule2) : ");
+            Console.WriteLine(r.IsMatch(Console.ReadLine()));
+        }
+    }
+}
